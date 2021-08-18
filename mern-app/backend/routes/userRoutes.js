0d4d7f80dbcc03c5a5c947af4b094c88b5ff03a8
Anouar-Tobji <82318881+Anouar-Tobji@users.noreply.github.com>
2021-08-18 17:12:32 +0100

    Delete mern-app directory

--- mern-app/backend/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require ('express')
-const router = express.Router()
-const {registerUser,authUser,updateUserProfile} =require ("../controllers/userControllers.js");
-const{protect} = require ("../middleware/authMiddleware")
-router.route("/").post(registerUser);
-router.route("/login").post(authUser);
-router.route("/profile").post(protect,updateUserProfile)
-module.exports=router
