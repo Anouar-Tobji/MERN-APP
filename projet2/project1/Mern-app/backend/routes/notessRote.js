cbafca9fd2baa17f690829077f62e1c75936c6b0
Anouar-Tobji <82318881+Anouar-Tobji@users.noreply.github.com>
2021-08-19 17:05:45 +0100

    Update notessRote.js

--- projet2/project1/Mern-app/backend/routes/notessRote.js
+++ projet2/project1/Mern-app/backend/routes/notessRote.js
@@ -6,5 +6,17 @@ router.route("/").get((req,res)=>{
     Note.find()
     .then(foundNotes=>res.json(foundNotes))
 })
+router.delete("/:_id" ,(req ,res)=>{
+
+    const {_id} =req.params
+    
+    Note.remove({_id})
+    .then(note=>  res.send({msg :"list of note" , note}))
+    .catch(error => console.log(error))
+    })
+
+
+
+
 
 module.exports= router
