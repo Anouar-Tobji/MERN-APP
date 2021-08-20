8b6c42dcee9594e8940d997ce0e5fb633d50524a
Anouar-Tobji <82318881+Anouar-Tobji@users.noreply.github.com>
2021-08-20 15:24:03 +0100

    Update server.js

--- projet2/project1/Mern-app/backend/server.js
+++ projet2/project1/Mern-app/backend/server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 const  connectDB  = require('./config/db')
 const userRoutes= require ('./routes/userRoutes')
+const path =require ('path');
 const noteRoutes= require ('./routes/noteRoutes')
 const {notFound,errorHandler} = require("./middleware/errorMiddleware")
 const app = express()
@@ -24,6 +25,23 @@ app.use(express.json())
    app.use('/api/notes',noteRoutes)
    app.use('/api/notess',require("./routes/notessRote"))
    app.use('/api/useress',require("./routes/usressRotes"))
+
+
+
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/front-end/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "front-end", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+  
 
    app.use(notFound)
    app.use(errorHandler)
