17f9de0a7022a1934ebcbb80fd4c131f75566b46
Anouar-Tobji <82318881+Anouar-Tobji@users.noreply.github.com>
2021-08-20 15:57:10 +0100

    Update server.js

--- projet2/project1/Mern-app/backend/server.js
+++ projet2/project1/Mern-app/backend/server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 const  connectDB  = require('./config/db')
 const userRoutes= require ('./routes/userRoutes')
-const path =require ('path');
+// const path =require ('path');
 const noteRoutes= require ('./routes/noteRoutes')
 const {notFound,errorHandler} = require("./middleware/errorMiddleware")
 const app = express()
@@ -28,19 +28,19 @@ app.use(express.json())
 
 
 
-__dirname = path.resolve();
+// __dirname = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/front-end/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/front-end/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "front-end", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "front-end", "build", "index.html"))
+//   );
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running..");
+//   });
+// }
   
 
    app.use(notFound)
